dd06d806982d03697dc52e9f29af1e4d8752877d
dalcde <dalcde@yahoo.com.hk>
2014-04-28 17:22:01 +0800

    5L-ify

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -1,3 +1,23 @@
+const LOG2 = Math.log(2);
+const MAP_LIST = ["Michael See",
+		  "2009001",
+		  "2009011",
+		  "2009012",
+		  "2009023",
+		  "2009037",
+		  "2009044",
+		  "2009048",
+		  "2009050",
+		  "2009064",
+		  "2009076",
+		  "2009081",
+		  "2009086",
+		  "2009089",
+		  "2009104",
+		  "2009105",
+		  "2009107",
+		  "2008119"];
+
 function HTMLActuator() {
   this.tileContainer    = document.querySelector(".tile-container");
   this.scoreContainer   = document.querySelector(".score-container");
@@ -47,51 +67,41 @@ HTMLActuator.prototype.clearContainer = function (container) {
 };
 
 HTMLActuator.prototype.addTile = function (tile) {
-  var self = this;
+    var self = this;
 
-  var wrapper   = document.createElement("div");
-  var inner     = document.createElement("div");
-  var position  = tile.previousPosition || { x: tile.x, y: tile.y };
-  var positionClass = this.positionClass(position);
+    var wrapper   = document.createElement("div");
+    var position  = tile.previousPosition || { x: tile.x, y: tile.y };
+    var positionClass = this.positionClass(position);
 
-  // We can't use classlist because it somehow glitches when replacing classes
-  var classes = ["tile", "tile-" + tile.value, positionClass];
+    this.setClass(wrapper, positionClass, MAP_LIST[Math.log(tile.value)/LOG2]);
 
-  if (tile.value > 2048) classes.push("tile-super");
-
-  this.applyClasses(wrapper, classes);
-
-  inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
-
-  if (tile.previousPosition) {
-    // Make sure that the tile gets rendered in the previous position first
-    window.requestAnimationFrame(function () {
-      classes[2] = self.positionClass({ x: tile.x, y: tile.y });
-      self.applyClasses(wrapper, classes); // Update the position
-    });
-  } else if (tile.mergedFrom) {
-    classes.push("tile-merged");
-    this.applyClasses(wrapper, classes);
-
-    // Render the tiles that merged
-    tile.mergedFrom.forEach(function (merged) {
-      self.addTile(merged);
-    });
-  } else {
-    classes.push("tile-new");
-    this.applyClasses(wrapper, classes);
-  }
-
-  // Add the inner part of the tile to the wrapper
-  wrapper.appendChild(inner);
-
-  // Put the tile on the board
-  this.tileContainer.appendChild(wrapper);
+    if (tile.previousPosition) {
+	// Make sure that the tile gets rendered in the previous position first
+	window.requestAnimationFrame(function () {
+	    self.setClass(wrapper, self.positionClass({ x: tile.x, y: tile.y }), MAP_LIST[Math.log(tile.value)/LOG2]);
+	});
+    } else if (tile.mergedFrom) {
+	this.setClass(wrapper, positionClass, MAP_LIST[Math.log(tile.value)/LOG2], "tile-merged");
+	
+	// Render the tiles that merged
+	tile.mergedFrom.forEach(function (merged) {
+	    self.addTile(merged);
+	});
+    } else {
+	this.setClass(wrapper, positionClass, MAP_LIST[Math.log(tile.value)/LOG2], "tile-new");
+    }
+    
+    // Put the tile on the board
+    this.tileContainer.appendChild(wrapper);
 };
 
-HTMLActuator.prototype.applyClasses = function (element, classes) {
-  element.setAttribute("class", classes.join(" "));
+HTMLActuator.prototype.setClass = function (element, positionClass, number, extra) {
+    if (!extra) extra = "";
+    element.setAttribute("class", "tile " + positionClass + " " + extra);
+    element.style.backgroundImage = "url('http://s07.ssc.edu.hk/Pages/DownloadUserHeader.aspx?sno=" + number + "')";
+    element.style.backgroundSize = "contain";
+    element.style.backgroundRepeat = "no-repeat";
+    element.style.backgroundPosition = "center";
 };
 
 HTMLActuator.prototype.normalizePosition = function (position) {
